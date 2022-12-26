328d736d1b781bf35cc528b2628daac700b1f536
emmanuel-dev000 <emmanuelpanganwork@gmail.com>
2022-12-26 16:34:04 +0800

    Updated: added @annotations to Anime class

--- src/main/java/com/pangan/kissanime/anime/Anime.java
+++ src/main/java/com/pangan/kissanime/anime/Anime.java
@@ -1,6 +1,17 @@
 package com.pangan.kissanime.anime;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tblAnime")
 public class Anime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String name;
     private String description;
