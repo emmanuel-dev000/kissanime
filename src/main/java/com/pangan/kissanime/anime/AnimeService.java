a7740bb9e01cc2db03732dca266d3e178ea9dc0b
emmanuel-dev000 <emmanuelpanganwork@gmail.com>
2022-12-26 16:51:52 +0800

    Updated: changed old AnimeList to AnimeRepository

--- src/main/java/com/pangan/kissanime/anime/AnimeService.java
+++ src/main/java/com/pangan/kissanime/anime/AnimeService.java
@@ -1,5 +1,6 @@
 package com.pangan.kissanime.anime;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -10,8 +11,11 @@ public class AnimeService {
 
     private List<Anime> animeList = new ArrayList<Anime>();
 
+    @Autowired
+    private AnimeRepository animeRepository;
+
     public List<Anime> getAllAnime() {
-        if (animeList.isEmpty()) {
+        if (animeRepository.findAll().isEmpty()) {
             Anime tempAnime = new Anime();
             tempAnime.setName("Temp Anime");
             tempAnime.setDescription("Description");
@@ -21,27 +25,38 @@ public class AnimeService {
             };
             tempAnime.setGenre(genre);
 
-            animeList.add(tempAnime);
+            animeRepository.save(tempAnime);
         }
-        return animeList;
+//        return animeList;
+        return animeRepository.findAll();
     }
 
     public void addNewAnime(Anime newAnime) {
         if (newAnime == null)
             throw new NullPointerException();
 
-        animeList.add(newAnime);
+//        animeList.add(newAnime);
+        animeRepository.save(newAnime);
     }
 
     public void updateAnimeById(int id, Anime updatedAnime) {
         if (updatedAnime == null)
             throw new NullPointerException();
 
-        animeList.set(id, updatedAnime);
+//        animeList.set(id, updatedAnime);
+        Anime prevAnime = animeRepository.findById(id).orElseThrow(() -> new NullPointerException());
+        prevAnime.setName(updatedAnime.getName());
+        prevAnime.setGenre(updatedAnime.getGenre());
+        prevAnime.setDescription(updatedAnime.getDescription());
+        animeRepository.save(prevAnime);
     }
 
     public void updateAnimeName(int id, String animeName) {
-        animeList.get(id).setName(animeName);
+//        animeList.get(id).setName(animeName);
+        animeRepository
+                .findById(id)
+                .orElseThrow(() -> new NullPointerException())
+                .setName(animeName);
     }
 
     public void deleteAnimeById(int id) {
@@ -49,7 +64,8 @@ public class AnimeService {
         if (deleteAnime == null)
             throw new NullPointerException();
 
-        animeList.remove(deleteAnime);
+//        animeList.remove(deleteAnime);
+        animeRepository.delete(deleteAnime);
     }
 
 }
