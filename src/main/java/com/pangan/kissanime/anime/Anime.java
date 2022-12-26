a763f7d800663829e461daf2121f6bb6db8702da
emmanuel-dev000 <emmanuelpanganwork@gmail.com>
2022-12-26 16:41:04 +0800

    Updated: changed Anime class setters, getters and constructors to @annotations

--- src/main/java/com/pangan/kissanime/anime/Anime.java
+++ src/main/java/com/pangan/kissanime/anime/Anime.java
@@ -1,11 +1,12 @@
 package com.pangan.kissanime.anime;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Setter
+@Getter
 @Entity
 @Table(name = "tblAnime")
 public class Anime {
@@ -17,35 +18,4 @@ public class Anime {
     private String description;
     private String[] genre;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String[] getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String[] genre) {
-        this.genre = genre;
-    }
 }
